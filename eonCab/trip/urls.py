b79e27874874fb5e4d36e337e1bcfe62809e3b96
Nikhitha Madduri <maddurinikhitha0@gmail.com>
2024-12-13 13:50:14 +0000

    initial commit

---
+++ eonCab/trip/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('getprice', views.get_trip_price),
+    path('book', views.book_trip),
+    path('loadlocation', views.loadLocations),
+    path('history', views.get_trip_history),
+    path('payment/pay', views.pay_trip),
+    path('locations', views.get_trip_locations),
+    path('get-location-path', views.get_location_path),
+    path('nearby-famous-locations', views.get_nearby_famous_locations)
+]
